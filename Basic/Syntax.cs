0ec14a02d4941ba44b6ad08c91e1acdd765fdab9
prensi sahu <prensi2tap@gmail.com>
2025-02-23 10:51:40 +0530

    Update

--- Basic/Syntax.cs
+++ Basic/Syntax.cs
@@ -4,11 +4,12 @@ namespace HelloWorld    // Namespace is used to organize and manage code.
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I am Learning C#");
             Console.WriteLine("It is awesome!");
+            Console.WriteLine(3 + 3);   // You can also output numbers, and perform mathematical calculations.
         }
     }
 }
