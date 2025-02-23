ee64fd8cb74b00325b539cd7a2356e4169965788
prensi sahu <prensi2tap@gmail.com>
2025-02-24 00:35:04 +0530

    New class added

---
+++ Basic/TypeCasting-Day5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Type casting is when you assign a value of one data type to another type.
+In C#, there are two types of casting:  1) Implicit Casting, 2) Explicit Casting. */
+
+namespace Basic
+{   
+    internal class TypeCasting  
+    {
+        public void Implicit()
+        {
+            int myInt = 9;
+            double myDouble = myInt;  // Automatic casting: int to double.
+            // Implicit casting is done automatically when passing a smaller size type to a larger size type.
+            Console.WriteLine(myInt);
+            Console.WriteLine(myDouble);
+        }
+        public void Explicit()
+        {
+            double myDouble = 9.78;
+            int myInt = (int)myDouble;  // Manual casting: double to int.
+            // Explicit casting must be done manually by placing the type in parentheses in front of the value.
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myInt);
+        }
+
+        public void TypeConversion()
+        {
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+            Console.WriteLine(Convert.ToString(myInt));    // Convert int to string.
+            Console.WriteLine(Convert.ToDouble(myInt));    // Convert int to double.
+            Console.WriteLine(Convert.ToInt32(myDouble));  // Convert double to int.
+            Console.WriteLine(Convert.ToString(myBool));   // Convert bool to string.
+        }
+    }
+}
---
+++ Basic/UserInput-Day6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class UserInput
+    {
+        public void UserinputExample()
+        {
+             Console.Write("Enter your name: ");  // Type your name and press enter.
+             string Name = Console.ReadLine();  // Create a string variable and get user input from the keyboard and store it in the variable.
+
+             Console.WriteLine("Your name is: " + Name);  // Print the value of the variable (name), which will display the input value.
+        }
+    }
+}
