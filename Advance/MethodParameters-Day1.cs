d0cea0ec574c389e116e9a7c76363a9641c32eba
prensi <prensi2tap@gmail.com>
2025-02-26 17:33:27 +0530

    Add files via upload

---
+++ Advance/MethodParameters-Day1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance
+{
+    internal class MethodParameters
+    {
+        static void MyMethod2(string fname)
+        {
+            Console.WriteLine(fname + " Refsnes");
+        }
+
+        static void Main()
+        {
+            MyMethod2("Liam");
+            MyMethod2("Jenny");
+            MyMethod2("Anja");
+        }
+
+        // Liam Refsnes
+        // Jenny Refsnes
+        // Anja Refsnes
+    }
+}
