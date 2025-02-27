791fc60c0eb30de9099c112fc4486bf68c83846d
prensi <prensi2tap@gmail.com>
2025-02-27 22:58:15 +0530

    Add files via upload

---
+++ C# Methods/MultipleMethods-Day1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class MultipleMethods
+    {
+        //  Example 1
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
+
+        static void Main()
+        {
+            MyMethod();
+            MyMethod();
+            MyMethod();
+        }  
+    }
+}
